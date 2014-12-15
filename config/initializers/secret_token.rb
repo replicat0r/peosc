ca371ebb4d4dd2bbf31c4c4dd20764406b2b2f03
tyler liu <replicat0r.misc@gmail.com>
2014-12-14 19:14:44 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peosc::Application.config.secret_key_base = '5aa1ff3a18c46bc9dad99884d2e8a3d429e8ce1beab57cd3df3631d2300297f7b6b2f2c6017754117fb9f1bff4be623d054fd1d427ee2dd3b7d913d847c7f7ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Peosc::Application.config.session_store :cookie_store, key: '_peosc_session'
